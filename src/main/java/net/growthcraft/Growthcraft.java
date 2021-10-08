fd8d38669072c5e4381e2418743ff3e94b892756
Falkor220 <Falkor220@gmail.com>
2021-10-08 17:48:37 +0100

    fixing a few things

--- src/main/java/net/growthcraft/Growthcraft.java
+++ src/main/java/net/growthcraft/Growthcraft.java
@@ -8,7 +8,7 @@ public class Growthcraft implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Logger LOGGER = LogManager.getLogger("growthcraft");
 
 	@Override
 	public void onInitialize() {
