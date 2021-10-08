64cfcc16f5f24dffbccafa5632d25469c985f92a
Falkor220 <Falkor220@gmail.com>
2021-10-08 22:02:15 +0100

    start of cheese

---
+++ src/main/java/net/growthcraft/blocks/AgedCheeseBlock.java
@@ -0,0 +1,9 @@
+package net.growthcraft.blocks;
+
+import net.minecraft.block.SlabBlock;
+
+public class AgedCheeseBlock extends SlabBlock {
+    public AgedCheeseBlock(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/net/growthcraft/items/GrowthcraftItems.java
@@ -0,0 +1,24 @@
+package net.growthcraft.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.growthcraft.Growthcraft;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.SimpleRegistry;
+
+import static net.growthcraft.Growthcraft.MOD_ID;
+
+public class GrowthcraftItems {
+
+
+
+
+    public static final Item RED_WAX = registerItem(new WaxItem(new Item.Settings().group(Growthcraft.ITEMGROUP)), "red_wax");
+
+    public static void register(){}
+
+    public static Item registerItem(Item item,String id) {
+        return SimpleRegistry.register(Registry.ITEM, new Identifier(MOD_ID,id), item);
+    }
+}
