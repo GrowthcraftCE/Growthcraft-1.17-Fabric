26a4e59464f272f689267a2f2bdec7d5a6ab97e6
falkory <falkor220@gmail.com>
2021-10-30 15:04:46 +0100

    Added more wax

--- src/main/java/net/growthcraft/items/GrowthcraftItems.java
+++ src/main/java/net/growthcraft/items/GrowthcraftItems.java
@@ -15,10 +15,14 @@ import java.util.List;
 import static net.growthcraft.Growthcraft.MOD_ID;
 
 public class GrowthcraftItems {
-    public static final Item RED_WAX = registerItem(new WaxItem(new Item.Settings().group(defaultGroup())).color(DyeColor.RED), "red_wax");
-    public static final Item CUTTING_KNIFE = registerItem(new KnifeItem(new Item.Settings().group(defaultGroup())), "cutting_knife");
-    public static final Item CHEDDAR_SLICE  = registerItem(new Item(new Item.Settings().group(defaultGroup()).food(new FoodComponent.Builder().hunger(1000).saturationModifier(1000).build())), "cheddar_slice");
-    public static final Item GORGONZOLA_SLICE  = registerItem(new Item(new Item.Settings().group(defaultGroup()).food(new FoodComponent.Builder().hunger(1000).saturationModifier(1000).build())), "gorgonzola_slice");
+    public static final Item RED_WAX = registerItem(new WaxItem(new Item.Settings().group(Growthcraft.ITEMGROUP)).color(DyeColor.RED), "red_wax");
+    public static final Item WHITE_WAX = registerItem(new WaxItem(new Item.Settings().group(Growthcraft.ITEMGROUP)).color(DyeColor.WHITE), "white_wax");
+    public static final Item BLACK_WAX = registerItem(new WaxItem(new Item.Settings().group(Growthcraft.ITEMGROUP)).color(DyeColor.BLACK), "black_wax");
+    public static final Item YELLOW_WAX = registerItem(new WaxItem(new Item.Settings().group(Growthcraft.ITEMGROUP)).color(DyeColor.YELLOW), "yellow_wax");
+    public static final Item CUTTING_KNIFE = registerItem(new KnifeItem(new Item.Settings().group(Growthcraft.ITEMGROUP)), "cutting_knife");
+    public static final Item CHEDDAR_SLICE  = registerItem(new Item(new Item.Settings().group(Growthcraft.ITEMGROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(2).build())), "cheddar_slice");
+    public static final Item GORGONZOLA_SLICE  = registerItem(new Item(new Item.Settings().group(Growthcraft.ITEMGROUP).food(new FoodComponent.Builder().hunger(6).saturationModifier(2).build())), "gorgonzola_slice");
+    public static final List<Item> CREATIVE_TAB_ITEMS = new ArrayList<>();
     
     public static void register(){}
 
@@ -27,7 +31,7 @@ public class GrowthcraftItems {
             return SimpleRegistry.register(Registry.ITEM, new Identifier(MOD_ID,id), item);
         else return item;
     }
-    
+
     public static ItemGroup defaultGroup(){
         if (GrowthCraftConstants.isGameRunning) return Growthcraft.ITEMGROUP; else return null;
     }
